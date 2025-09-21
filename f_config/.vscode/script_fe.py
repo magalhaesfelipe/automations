50df882d0e9bfc534ad9c48d8beff3d1e14234da
zer0 <ffelipemagallhaes@gmail.com>
2025-09-20 23:47:32 -0400

    feat: add denpendecy solving automation

--- f_config/.vscode/script_fe.py
+++ f_config/.vscode/script_fe.py
@@ -2,7 +2,16 @@
 import os
 import subprocess
 
-dirs = ["home", "triagem", "login", "orcamento", "root", "config", "cadastro", "agenda"]
+dirs = [
+    "app-home",
+    "app-triagem",
+    "app-login",
+    "app-orcamento",
+    "app-root",
+    "app-config",
+    "app-cadastro",
+    "app-agenda",
+]
 
 
 for d in dirs:
@@ -14,7 +23,9 @@ for d in dirs:
         print(f"Skipping {d} (not a git repo)")
         continue
 
+    print()
     print(f"Updating {d}...")
+    print()
     try:
         subprocess.run(["git", "-C", d, "pull"], check=True)
     except subprocess.CalledProcessError:
@@ -24,6 +35,27 @@ print()
 print(f"##### Finished updates #####")
 print()
 
+print()
+print(f"#### Installing packages ####")
+print()
+
+for d in dirs:
+    if not os.path.isdir(d):
+        print(f"Directory {d} does not exist. Skipping...")
+        continue
+
+    if not os.path.isfile(os.path.join(d, "package.json")):
+        print(f"Skipping {d} (no package.json config found)")
+        continue
+
+    print()
+    print(f"Installing packages in {d}...")
+    print()
+    try:
+        subprocess.run(["npm", "install"], cwd=d, check=True)
+    except subprocess.CalledProcessError:
+        print(f"Failed to install packages in {d}")
+
 print(f"Launching")
-subprocess.run(["npm", "start"], shell=True, check=True)
+subprocess.run(["npm", "start"], check=True)
 print()
